3a3df3fee57e7060817b9f03a63c833805522739
MAT-services <mat.stocage@gmail.com>
2026-01-10 15:16:06 +0100

    Update sudo.bat

--- sudo.bat
+++ sudo.bat
@@ -1,14 +1,13 @@
 @echo off
 if "%1"=="" goto noArg
 if NOT EXIST %1 goto notExe
-set process="Start-Process %1 -Verb RunAs"
+set process="Start-Process %* -Verb RunAs"
 goto exec
 :notExe
-set process="Start-Process cmd -Verb RunAs -ArgumentList '/c %1'"
+set process="Start-Process cmd -Verb RunAs -ArgumentList '/c %*'"
 goto exec 
+:exec
+powershell -c %process%
+goto :eof
 :noArg
 echo No arguments found. 
-goto :eof
-:exec
-
-powershell -c %process%
