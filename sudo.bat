897d4acbe2e149d4591f39b7be4763925782bd76
MAT Services Administrator <157519566+MAT-services@users.noreply.github.com>
2026-01-10 13:46:29 +0100

    Repairing Start-Process arguments

--- sudo.bat
+++ sudo.bat
@@ -1,13 +1,14 @@
 @echo off
 if "%1"=="" goto noArg
 if NOT EXIST %1 goto notExe
-set process=%1
+set process="Start-Process %1 -Verb RunAs"
 goto exec
 :notExe
-set process="cmd -c %1"
+set process="Start-Process cmd -Verb RunAs -ArgumentList '/c %1'"
 goto exec 
 :noArg
 echo No arguments found. 
 goto :eof
 :exec
-powershell -c Start-process %process% -Verb RunAs
+
+powershell -c %process%
